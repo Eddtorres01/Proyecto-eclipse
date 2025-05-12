2dbb6f092532a1e36572752004255e96642af0bf
Eddtorres01 <tp21002@ues.edu.sv>
2025-05-11 18:15:07 -0600

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,21 @@
+package com.iis.tp21002;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello() {
+        assertTrue(true);
+    }
+}
